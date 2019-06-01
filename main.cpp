912080ffed198b5692e871a56d3d35172ab14936
ng91 <nikoloz.glonti@o2.pl>
2019-06-01 12:24:24 +0200

    pierwszy commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : zajecia1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+
+
+
+int main() {
+
+while(1){
+	cout << "nikoloz" << endl; // prints !!!Hello World!!!
+
+}
+
+}
